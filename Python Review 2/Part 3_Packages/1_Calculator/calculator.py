e57ea86eb6bf3201e2896dd08fff94d176a9dd6a
sullivaneg <emma.sullivan@mymail.champlain.edu>
2025-09-30 15:33:37 -0400

    "Part 3 Packages"

---
+++ Python Review 2/Part 3_Packages/1_Calculator/calculator.py
@@ -0,0 +1,35 @@
+def sum(a, b):
+    """
+    This function calculate the sum of two numbers.
+    :param a: first number
+    :param b: second number
+    :return: sum of both numbers
+    """
+    return a + b
+
+def subtraction(a, b):
+    """
+    This function calculate the difference of two numbers.
+    :param a: first number
+    :param b: second number
+    :return: difference of both numbers
+    """
+    return a - b
+
+def multiplication(a, b):
+    """
+    This function calculate the product of two numbers.
+    :param a: first number
+    :param b: second number
+    :return: the product of both numbers
+    """
+    return a * b
+
+def division(a, b):
+    """
+    This function calculate the quotient of two numbers.
+    :param a: first number
+    :param b: second number
+    :return: the quotient of both numbers
+    """
+    return a / b
---
+++ Python Review 2/Part 3_Packages/1_Calculator/calculatorCaller.py
@@ -0,0 +1,10 @@
+from calculator import *
+
+num_a = float(input("Enter the first number: "))
+num_b = float(input("Enter the second number: "))
+
+print("______________________________\nCALCULATOR\n____________________________")
+print('SUM = ', sum(num_a, num_b))
+print('PRODUCT = ', multiplication(num_a, num_b))
+print("SUBTRACTION = ", subtraction(num_a, num_b))
+print("DIVISION = ", division(num_a, num_b))

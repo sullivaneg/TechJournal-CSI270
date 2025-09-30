ff281d47fd98d1c496bc2728f8c124ac87bba02a
sullivaneg <emma.sullivan@mymail.champlain.edu>
2025-09-30 16:03:25 -0400

    "Part 3 Packages"

---
+++ Python Review 2/Part 3_Packages/2_InputModifier/inputModifier.py
@@ -0,0 +1,23 @@
+def inputString(message):
+    """
+     this function is used to read a string value from the user and return that value.
+    :return: string
+    """
+    string = input(message)
+    return string
+
+def inputInt(message):
+    """
+    this function is used to read an int value from the user and return that value.
+    :return:
+    """
+    integer = int(input(message))
+    return integer
+
+def inputFloat(message):
+    """
+    this function is used to read a float value from the user and return that value.
+    :return:
+    """
+    integer = float(input(message))
+    return integer
---
+++ Python Review 2/Part 3_Packages/2_InputModifier/inputModifierTest.py
@@ -0,0 +1,24 @@
+from inputModifier import *
+def output(name, sum):
+    """
+    This function is just to store the print output
+    :return: prints the output
+    """
+    print("______________________________\nInput Modifier Test\n______________________________")
+    print("Process Name: ", name)
+    print("Sum of all consumed memory: ", sum)
+
+def main():
+    """
+    This is the main function.
+    :return: Takes user input and calls output() to print out the output.
+    """
+    process = inputString("Please input the process name: ")
+    thread_num = inputInt("Please input the number of threads: ")
+    memory_count = 0
+    for i in range(thread_num):
+        memory = inputFloat(f'Please input the memory size for thread {i+1}: ')
+        memory_count += memory
+    output(process, memory_count)
+
+main()
